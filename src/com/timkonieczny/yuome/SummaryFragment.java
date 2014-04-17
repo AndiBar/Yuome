029c0be46d7034c8852354a7c73f58d1539d8206
timbotombo <timkonieczny@gmail.com>
2014-04-17 17:36:24 +0200

    Canvas im SummaryFragment hinzugefügt.

--- src/com/timkonieczny/yuome/SummaryFragment.java
+++ src/com/timkonieczny/yuome/SummaryFragment.java
@@ -1,21 +1,39 @@
 package com.timkonieczny.yuome;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class SummaryFragment extends Fragment {
-
-	public SummaryFragment() {
-		// Empty constructor required for fragment subclasses
-	}
-
-	@Override
+    
+    
+    public SummaryFragment(){
+    	
+    }
+    
+    @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		View rootView = inflater.inflate(R.layout.fragment_summary, container, false);
+		
+		Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#CD5C5C"));
+        Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bg);
+        canvas.drawRect(50, 50, 200, 200, paint);
+        LinearLayout ll = (LinearLayout) rootView.findViewById(R.id.rect);
+        ll.setBackgroundDrawable(new BitmapDrawable(bg));
+		
 		return rootView;
 	}
 }
