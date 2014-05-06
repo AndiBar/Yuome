e4a5ad72f77688a1c6d67a86aad99f3de3441f72
timbotombo <timkonieczny@gmail.com>
2014-05-06 20:41:23 +0200

    ListView zum ReceiptsFragment hinzugefügt (muss noch richtig formatiert werden)

--- src/com/timkonieczny/yuome/ReceiptsFragment.java
+++ src/com/timkonieczny/yuome/ReceiptsFragment.java
@@ -1,12 +1,14 @@
 package com.timkonieczny.yuome;
 
-import android.app.Fragment;
+import java.util.ArrayList;
+
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ReceiptsFragment extends Fragment { // jeweils eine Klasse für jedes Fragment erstellen
+public class ReceiptsFragment extends ListFragment { // jeweils eine Klasse für jedes Fragment erstellen
 
 	public ReceiptsFragment() {
 		// Empty constructor required for fragment subclasses
@@ -15,6 +17,15 @@ public class ReceiptsFragment extends Fragment { // jeweils eine Klasse für jede
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_receipts, container, false);
+		ReceiptAdapter adapter = new ReceiptAdapter(rootView.getContext(), fillReceiptsList());
+		setListAdapter(adapter);
 		return rootView;
+	}
+	
+	private ArrayList<Receipt> fillReceiptsList(){	// TODO: ggf. Vorhandene Einkäufe beim ersten Start der App herunterladen
+		ArrayList<Receipt> receiptsList = new ArrayList<Receipt>();
+		receiptsList.add(new Receipt("01.01.2014","Aldi","Erik, Andi","15,63"));
+	    
+	    return receiptsList;
 	}
 }
