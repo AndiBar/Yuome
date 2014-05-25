8f317c4c22a1ddf3c6e1817b87a06155fa0733c6
Harbeckster <town_war@gmx.de>
2014-05-25 18:32:35 +0200

    Kontakt-Liste hinzugefügt
    Kontaktliste im ConctactsFragment, Funktion fehlt noch

--- src/com/timkonieczny/yuome/ContactsFragment.java
+++ src/com/timkonieczny/yuome/ContactsFragment.java
@@ -1,9 +1,15 @@
 package com.timkonieczny.yuome;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+
+import org.apache.http.client.ClientProtocolException;
+
+import com.timkonieczny.yuome.ChooseContactsActivity.FriendsThread;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,24 +18,47 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
 
-public class ContactsFragment extends Fragment {
-
+public class ContactsFragment extends ListFragment {
+    public static ArrayList<HashMap<String, String>> friends_list = new ArrayList<HashMap<String,String>>();
 	private ListView settings;
+    public static ContactsFragmentAdapter mAdapter;
+
 
 	public ContactsFragment() {
 		// Empty constructor required for fragment subclasses
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Thread friends_thread = new FriendsThread();
+        friends_thread.start();
+        
+        try {
+        	long waitMillis = 10000;
+        	while (friends_thread.isAlive()) {
+        	   friends_thread.join(waitMillis);
+        	}
+        } catch (InterruptedException e) {
+        	}
+        
+        mAdapter = new ContactsFragmentAdapter(getActivity(),
+        		friends_list,
+        		 R.layout.fragment_contacts_item,
+                 new String[] {"title"},
+                 new int[] {R.id.title});
+        
+        ListView listView = getListView();
+        setListAdapter(mAdapter);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		setHasOptionsMenu(true);	
 		View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
-		
+	
 		return rootView;
 	}
 	
@@ -53,8 +82,17 @@ public class ContactsFragment extends Fragment {
 	    }
 	}
 	  
-
-
-
-
+	public class FriendsThread extends Thread{
+  	  public void run(){
+  		  try {
+  			  friends_list = PHPConnector.getData("get_friends.php");
+  		  } catch (ClientProtocolException e) {
+  			  // TODO Auto-generated catch block
+  			  e.printStackTrace();
+  		  } catch (IOException e) {
+  			  // TODO Auto-generated catch block
+  			  e.printStackTrace();
+  		  }
+  	  }
+	}
 }
---
+++ src/com/timkonieczny/yuome/ContactsFragmentAdapter.java
@@ -0,0 +1,56 @@
+package com.timkonieczny.yuome;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class ContactsFragmentAdapter extends SimpleAdapter {
+    private ArrayList<String> checked_user_ids;
+    private LayoutInflater inflater;
+    public ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String,String>>();
+
+	public ContactsFragmentAdapter(Context context,
+			List<? extends Map<String, ?>> data, int resource, String[] from,
+			int[] to) {
+		super(context, data, resource, from, to);
+		this.inflater = LayoutInflater.from(context);
+		this.checked_user_ids = new ArrayList<String>();
+		this.data = (ArrayList<HashMap<String, String>>) data;
+		// TODO Auto-generated constructor stub
+	}
+
+	public View getView(final int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.fragment_contacts_item, null);
+        }
+        //final CheckBox check_box = (CheckBox) convertView.findViewById(R.id.contactCheckBox);
+        final TextView user_view = (TextView) convertView.findViewById(R.id.title);
+        user_view.setText(data.get(position).get("title"));
+      
+		user_view.setOnClickListener(new OnClickListener() {
+			    @Override
+	        public void onClick(View v) {
+
+	            if(checked_user_ids.contains(data.get(position).get("ID"))){
+	            	checked_user_ids.remove(data.get(position).get("ID"));
+	            }
+	        }
+	    });
+	       
+	    return convertView;
+    }
+	public ArrayList<String> getCheckedUserIDs(){
+		return checked_user_ids;
+	}
+}
